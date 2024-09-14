8812789564ea8d81b800e0216e88b595cc971d44
Yunus Emre Uçar <yunusemreucr11@gmail.com>
2024-09-14 23:48:16 +0300

    isim resim vebazı stiller eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import Myself from "./assets/myself.jpeg"
 
 function App() {
 
   return (
-    <div>
-      <h1 className='text-red-500'>Hello there! wow</h1>
+    <div className="flex items-center justify-center max-w-7xl min-h-screen mx-auto bg-red-50">
+      <div>
+        <div className="flex flex-col items-center gap-4 border-4 p-16 hover:bg-gray-300 cursor-pointer transition border-gray-800 rounded-lg">
+          <img
+            className="w-60 h-60 object-cover rounded-full"
+            src={Myself}
+            alt="asdad"
+          />
+          <div className="text-center">
+            <span className="text-5xl font-extrabold">Yunus Emre Uçar</span>
+            <p className="text-xl font-bold text-gray-700">FullStack Developer</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
