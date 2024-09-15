e9e6fa14ccc8f26e1c27ab9befe2de66fb064698
Yunus Emre Uçar <yunusemreucr11@gmail.com>
2024-09-15 23:41:51 +0300

    wow

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-import AnimatedSection from "./AnimatedSection";
 
 function App() {
 
 
   return (
     <div>
-      <RightCards />
       <div className="flex items-center justify-center max-w-7xl min-h-screen mx-auto">
         <div className="flex gap-3">
-          <AnimatedSection />
+          Selam
         </div>
       </div>
     </div>
