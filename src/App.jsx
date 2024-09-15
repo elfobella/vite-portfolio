ed396711f697a5ef445aec0991e5f3a34b2567d3
Yunus Emre Uçar <yunusemreucr11@gmail.com>
2024-09-15 12:56:50 +0300

    socials and animated cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
+import { useState } from "react";
+import AnimatedSection from "./AnimatedSection";
 import Myself from "./assets/myself.jpeg"
+import LeftCards from "./LeftCards";
+import RightCards from "./RightCards";
+import Socials from "./Socials";
 
 function App() {
 
+  const [logoText,setLogoText] = useState("")
+
   return (
-    <div className="flex items-center justify-center max-w-7xl min-h-screen mx-auto bg-red-50">
-      <div>
-        <div className="flex flex-col items-center gap-4 border-4 p-16 hover:bg-gray-300 cursor-pointer transition border-gray-800 rounded-lg">
-          <img
-            className="w-60 h-60 object-cover rounded-full"
-            src={Myself}
-            alt="asdad"
-          />
-          <div className="text-center">
-            <span className="text-5xl font-extrabold">Yunus Emre Uçar</span>
-            <p className="text-xl font-bold text-gray-700">FullStack Developer</p>
-          </div>
+    <div>
+      <RightCards />
+      <div className="flex items-center justify-center max-w-7xl min-h-screen mx-auto">
+        <div className="flex gap-3">
+          <AnimatedSection />
         </div>
       </div>
     </div>
