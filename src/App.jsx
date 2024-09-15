bbc37a15e632e47bc09c9ecdfe0286cf074a6019
Yunus Emre Uçar <yunusemreucr11@gmail.com>
2024-09-15 23:34:02 +0300

    new1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import mySelf from "./assets/Portfolio-image.jpeg"
 
 function App() {
 
   return (
-    <div>
-      <h1>Hello there!</h1>
+    <div className="max-w-7xl mx-auto flex items-center justify-center min-h-screen">
+      <div>
+        <img src={mySelf} className="w-60 h-60 object-cover rounded-full" alt="" />
+        <div>
+          <h3 className="text-5xl font-bold">Yunus Emre UÇAR</h3>
+          <h3 className="">FullStack Developer</h3>
+        </div>
+      </div>
     </div>
   )
 }
