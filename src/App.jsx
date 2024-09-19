1ae316e8c57d3e6c759ead1dce658ee06eb88fe3
elfobella <yunusemreucr11@gmail.com>
2024-09-19 21:50:10 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-
 function App() {
   return (
     <div>
       <div className="flex items-center justify-center max-w-7xl min-h-screen mx-auto">
         <div className="flex gap-3">Selam</div>
-        <div>yanlış</div>
       </div>
     </div>
   );
